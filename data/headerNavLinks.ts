f6df9b150208f4f1e6b00f46c765005116853485
DucBM@reifrontier.jp <linhhv@nlbn.net>
2024-05-21 17:46:14 +0700

    New theme

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/video', title: 'Video' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
---
+++ src/components/analytics/index.tsx
@@ -0,0 +1,22 @@
+import GA from './GoogleAnalytics'
+import siteMetadata from '@/data/siteMetadata'
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void
+    plausible?: (...args: any[]) => void
+    sa_event?: (...args: any[]) => void
+  }
+}
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const Analytics = () => {
+  return (
+    <>
+      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+    </>
+  )
+}
+
+export default Analytics
